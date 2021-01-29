cb6e753641af45fe209443ab04ca08f94373343a
ray <33544674+conver4y@users.noreply.github.com>
2021-01-30 02:01:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ require("./handler/Event.js")(client);
 
 require('dotenv').config(); // npm install dotenv
 
-client.package = require("./package.json");
 client.on("warn", console.warn); // This will warn you via logs if there was something wrong with your bot.
 client.on("error", console.error); // This will send you an error message via logs if there was something missing with your coding.
 client.login(process.env.SECRET).catch(console.error); // This token will leads to the .env file. It's safe in there.
