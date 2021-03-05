e5d6d30dc3d69b1cfd87ed0f28e6d38046781585
ray <33544674+conver4y@users.noreply.github.com>
2021-03-05 14:12:36 +0800

    Update ping.js

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,18 +1,15 @@
-// const Discord = require('discord.js');
-
 exports.run = async (client, message, args) => {
   message.channel.send("Pong!");
 };
 
 exports.help = {
   name: "ping",
-  description: "Ponged!",
+  description: "Check if the bot was online.",
   usage: "!ping",
   example: "!ping"
 };
 
 exports.conf = {
-  aliases: ["beep"],
-  cooldown: 5 // This number is a seconds, not a milliseconds.
-  // 1 = 1 seconds.
+  aliases: [],
+  cooldown: 5 // Integer = second.
 };
